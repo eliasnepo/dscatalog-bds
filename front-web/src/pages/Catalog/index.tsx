0471fb5a22ded79444076b0fdb261a521e71293c
Elias Nepomuceno <eliasnepo12@gmail.com>
2021-01-21 14:20:09 -0300

    Finish product card

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import ProductCard from './components/ProductCard';
+import './styles.scss';
 
 function Catalog() {
     return (
-        <div>
-            <h1>Catalog</h1>
+        <div className="catalog-container">
+            <h1 className="catalog-title">
+            Catálogo de produtos
+            </h1>
+            <div className="catalog-products">
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+            </div>
         </div>
     )
 }
--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 function Home() {
     return (
         <div className="home-container">
-            <div className="row home-content">
+            <div className="row home-content card-base border-radius-20">
                 <div className="col-6">
                     <h1 className="text-title">Conheça o melhor <br/>catálogo de produtos</h1>
                     <p className="text-subtitle">
@@ -18,7 +18,7 @@ function Home() {
                     </Link>
                 </div>
                 <div className="col-6">
-                    <MainImage className="main-image" />
+                    <MainImage className="home-image" />
                 </div>
             </div>
         </div>
