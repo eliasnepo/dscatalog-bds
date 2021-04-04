87fa333ba37542c90b220a4d9e4a001708e67f86
Elias Nepomuceno <eliasnepo12@gmail.com>
2021-04-04 12:32:36 -0300

    Fixed

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './core/assets/styles/custom.scss';
-import './app.scss';
+import './App.scss';
 import Routes from './Routes';
 
 const App = () => {
